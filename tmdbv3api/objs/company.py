d27c813fab51e9753c0ae329b1dcc4cf90f7ad05
xyp8023 <xyp8023@gmail.com>
2020-12-03 14:54:54 +0100

    added search company's id for its name

--- tmdbv3api/objs/company.py
+++ tmdbv3api/objs/company.py
@@ -1,9 +1,13 @@
 from tmdbv3api.tmdb import TMDb
-from tmdbv3api.as_obj import AsObj
+from tmdbv3api.as_obj import AsObj, AsObj_id
+try:
+    from urllib import quote
+except ImportError:
+    from urllib.parse import quote
 
 
 class Company(TMDb):
-    _urls = {"details": "/company/%s", "movies": "/company/%s/movies"}
+    _urls = {"details": "/company/%s", "movies": "/company/%s/movies", "search_company": "/search/company",}
 
     def details(self, company_id):
         """
@@ -20,3 +24,25 @@ class Company(TMDb):
         :return:
         """
         return self._get_obj(self._call(self._urls["movies"] % str(company_id), ""))
+    
+    @staticmethod
+    def _get_obj_id(result, key="results"):
+        if "success" in result and result["success"] is False:
+            raise Exception(result["status_message"])
+        arr = []
+        if key is not None:
+            [arr.append(AsObj_id(**res)) for res in result[key]]
+        else:
+            return result
+        return arr
+
+    def search_id(self, term, page=1):
+        """
+        Search for company.
+        :param term: string
+        :param page: int
+        :return: id
+        """
+        return self._get_obj_id(self._call(
+                self._urls["search_company"],
+                "query=" + quote(term) + "&page=" + str(page),))
